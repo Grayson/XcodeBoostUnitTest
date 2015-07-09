b74c4d7e42a8745b071c15a028c1e2b453ced097
Grayson <info@fromconcentratesoftware.com>
2015-07-08 20:05:22 -0700

    Adding basic library files for testing.

---
+++ TestLibrary/TestLibrary/PublicFunctions.cpp
@@ -0,0 +1,7 @@
+#include "PublicFunctions.h"
+
+namespace PublicFunctions
+{
+	int GetOne() { return 1; }
+	int GetTwo() { return 2; }
+}
---
+++ TestLibrary/TestLibrary/PublicFunctions.h
@@ -0,0 +1,5 @@
+namespace PublicFunctions
+{
+	int GetOne();
+	int GetTwo();
+}
